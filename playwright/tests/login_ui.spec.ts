5d28ab84b318eeeb99a74ada0aa40e7054485bc9
latkopp <96778190+latkopp@users.noreply.github.com>
2026-08-06 09:04:17 +0200

    Update login_ui.spec.ts

--- playwright/tests/login_ui.spec.ts
+++ playwright/tests/login_ui.spec.ts
@@ -4,20 +4,20 @@ import { ProductsPage } from '../pages/ProductsPage';
 
 test.describe('E-Commerce UI Login Tests (POM)', () => {
 
-    test('User should log in successfully with valid credentials', async ({ page }) => {
-      const loginPage = new LoginPage(page);
-      const productsPage = new ProductsPage(page);
+  test('User should log in successfully with valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const productsPage = new ProductsPage(page);
 
-      await loginPage.goto();
-      await loginPage.login('student', 'Password123');
-      await productsPage.verifyPageLoaded();
-    });
+    await loginPage.goto();
+    await loginPage.login('student', 'Password123');
+    await productsPage.verifyPageLoaded();
+  });
 
-      test('User should see error message with invalid credentials', async ({ page }) => {
-        const loginPage = new LoginPage(page);
+  test('User should see error message with invalid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
 
-        await loginPage.goto();
-        await loginPage.login('invalidUser', 'WrongPassword');
-        await loginPage.verifyErrorMessage('Your username is invalid!');
-      });
+    await loginPage.goto();
+    await loginPage.login('incorrectUser', 'Password123');
+    await loginPage.verifyErrorMessage('Your username is invalid!');
+  });
 });
