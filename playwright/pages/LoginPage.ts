1bd9662e2fa0dcc68f56dbcee45aab8fdb48a15f
latkopp <96778190+latkopp@users.noreply.github.com>
2026-08-06 07:47:53 +0200

    Create LoginPage.ts

---
+++ playwright/pages/LoginPage.ts
@@ -0,0 +1,29 @@
+import { Page, Locator, expect } from '@playwright/test';
+export class LoginPage {
+  readonly page: Page;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator('#username');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#submit');
+    this.errorMessage = page.locator('#error');
+  }
+
+  async goto() {
+    await this.page.goto('https://pracnique.github.io/practice-login/');
+  }
+
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async verifyErrorMessage(expectedText: string) {
+    await expect(this.errorMessage).toBeVisible();
+    await expect(this.errorMessage).toContainText(expectedText);
