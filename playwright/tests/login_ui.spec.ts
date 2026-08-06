7395b44a7d47bc9c3eb95b524e501dc654a45039
latkopp <96778190+latkopp@users.noreply.github.com>
2026-08-06 08:11:09 +0200

    Create login_ui.spec.ts

---
+++ playwright/tests/login_ui.spec.ts
@@ -0,0 +1,23 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { ProductsPage } from '../pages/ProductsPage';
+
+test.describe('E-Commerce UI Login Tests (POM)', () => {
+
+    test('User should log in successfully with valid credentials', async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      const productsPage = new ProductsPage(page);
+
+      await loginPage.goto();
+      await loginPage.login('student', 'Password123');
+      await productsPage.verifyPageLoaded();
+    });
+
+      test('User should see error message with invalid credentials', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+
+        await loginPage.goto();
+        await loginPage.login('invalidUser', 'WrongPassword');
+        await loginPage.verifyErrorMessage('Your username is invalid!');
+      });
+});
